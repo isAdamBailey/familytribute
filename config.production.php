60b751f89e0180701edb728a12994b7120cc1bdd
Adam Bailey <adamjbailey7@gmail.com>
2021-11-25 08:39:45 -0800

    initial landing page install

---
+++ config.production.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'baseUrl' => 'https://historyfam.com',
+    'production' => true,
+
+    // DocSearch credentials
+    'docsearchApiKey' => '',
+    'docsearchIndexName' => '',
+];
