52999f4af40963eadc5d3ca598ad8fde0065eeda
Adam Bailey <adamjbailey7@gmail.com>
2021-11-30 20:47:27 -0800

    style 404 page

--- source/404.blade.php
+++ source/404.blade.php
@@ -1,11 +1,14 @@
 @extends('_layouts.master')
 
 @section('body')
-<div class="flex flex-col items-center mt-32 text-gray-700">
-    <h1 class="text-6xl font-light leading-none mb-2">404</h1>
-
-    <h2 class="text-3xl">Page not found</h2>
+<div class="flex flex-col items-center">
+    <img class="w-1/3" src="/assets/img/undraw_page_not_found_re_e9o6.svg" alt="404 - Page Not Found">
+    <h2 class="text-2xl text-gray-700">Hmmmm...</h2>
 
     <hr class="block w-full max-w-lg mx-auto my-8 border">
+    <a href="/">
+        <x-button label="Back Home?"/>
+    </a>
+
 </div>
 @endsection
